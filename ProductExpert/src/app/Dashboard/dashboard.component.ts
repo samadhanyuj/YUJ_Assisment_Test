fea97ff41dbcf7166bc98e76b682cf53c4b3cebe
Samadhan <>
2021-12-29 17:10:28 +0530

    Working on the templete driven forms

--- ProductExpert/src/app/Dashboard/dashboard.component.ts
+++ ProductExpert/src/app/Dashboard/dashboard.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
