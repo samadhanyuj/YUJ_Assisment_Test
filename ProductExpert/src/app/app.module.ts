c39bdf5617a2732804fa3e47c2382b8c27cad058
Samadhan <>
2021-12-29 16:30:56 +0530

    Working on the generate dashbaord, footer, header components

---
+++ ProductExpert/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
+import { FooterComponent } from './Footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
